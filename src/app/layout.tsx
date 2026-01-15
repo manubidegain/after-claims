e5f3b0609346b66b26d495b8b73667002de553dd
manubidegain <jbidegainandrade@gmail.com>
2026-01-15 19:07:38 -0300

    change layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,22 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Chris Stussy Pop Up - Key Mood",
   description: "Inscripción para el Pop Up de Chris Stussy en Luxo, La Barra",
+  icons: {
+    icon: '/LogoKEY_Negro.png',
+    apple: '/LogoKEY_Negro.png',
+  },
+  openGraph: {
+    title: "Chris Stussy Pop Up - Key Mood",
+    description: "Registrate para el Pop Up de Chris Stussy el 17/01 en Luxo, La Barra",
+    images: ['/LogoKEY_Negro.png'],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: "Chris Stussy Pop Up - Key Mood",
+    description: "Registrate para el Pop Up de Chris Stussy el 17/01 en Luxo, La Barra",
+    images: ['/LogoKEY_Negro.png'],
+  },
 };
 
 export default function RootLayout({
