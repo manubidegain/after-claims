a63035b9d6c2dba657628cfc7ecd9ec16f735bd4
manubidegain <jbidegainandrade@gmail.com>
2026-01-15 19:35:15 -0300

    remove header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,14 +40,6 @@ export default function Home() {
       <div className="absolute top-1/4 left-1/4 w-96 h-96 bg-purple-300/10 rounded-full blur-3xl"></div>
       <div className="absolute bottom-1/4 right-1/4 w-96 h-96 bg-orange-300/10 rounded-full blur-3xl"></div>
 
-      {/* Header */}
-      <div className="relative z-10 pt-8 pb-4">
-        <div className="container mx-auto px-4">
-          <div className="flex justify-between items-start">
-          </div>
-        </div>
-      </div>
-
       {/* Main Content */}
       <div className="relative z-10 flex-1 flex items-center justify-center px-4 py-12">
         <div className="w-full max-w-4xl">
